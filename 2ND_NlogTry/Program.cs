3f74f973e4c124b49a570a4c05ac21d8d71e9b78
henimex <49072861+henimex@users.noreply.github.com>
2021-01-16 14:56:57 +0300

    Add project files.

---
+++ 2ND_NlogTry/Program.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ND_NlogTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Logger = NLog.LogManager.GetCurrentClassLogger();
+            Logger.Log(LogLevel.Info, $"Proje Başladı {DateTime.Now.ToString("dd.MM.yyyy hh:ss")}");
+            Logger.Log(LogLevel.Warn, $"Warning Mesajı {DateTime.Now.DayOfYear.ToString()}");
+            Logger.Log(LogLevel.Info, $"Proje Bitti {DateTime.Now.ToString("dd.MM.yyyy hh:ss")}");
+        }
+    }
+}
